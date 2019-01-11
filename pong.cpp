47fce0694f96924d1e17611d4911cb5a23e7c516
Federico Gerardi <38018471+PyGera@users.noreply.github.com>
2019-01-11 20:33:04 +0100

    Update pong.cpp

--- pong.cpp
+++ pong.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 //Dichiarazione Variabili
 
-int yHome = 0, xHome = 0, yBall = 10, xBall = 10, i, a;
-const char* campo[20][20];
+int yHome = 0, xHome = 0, yBall = 10, xBall = 10, i, a , yVisitors = 0, xVisitors = 0;
+const char* campo[20][70];
 
 //Funzione per la palla
 //TODO: Completarla
@@ -22,9 +22,8 @@ void ball() {
 //Funzione per il giocatore di casa
 
 void home() {
-	campo[0][yHome] = " ";
-	if (GetKeyState(VK_DOWN) & 0x8000 && yHome < 16) yHome++; //Pressione Freccia Giù e spostamento in basso
-	if (GetKeyState(VK_UP) & 0x8000 && yHome > 0) yHome--; //Pressione Freccia Su e spostamento in alto non oltre lo 0 (quindi il bordo della finestra)
+	if (GetKeyState('S') & 0x8000 && yHome < 16) yHome++; //Pressione Freccia Giù e spostamento in basso
+	if (GetKeyState('W') & 0x8000 && yHome > 0) yHome--; //Pressione Freccia Su e spostamento in alto non oltre lo 0 (quindi il bordo della finestra)
 	campo[yHome][0] = "|";
 	campo[yHome + 1][0]= "|";
 	campo[yHome + 2][0] = "|";
@@ -32,26 +31,37 @@ void home() {
 	_getch();
 }
 
+void visitors() {
+	if (GetKeyState(VK_DOWN) & 0x8000 && yVisitors < 16) yVisitors++; //Pressione Freccia Giù e spostamento in basso
+	if (GetKeyState(VK_UP) & 0x8000 && yVisitors > 0) yVisitors--; //Pressione Freccia Su e spostamento in alto non oltre lo 0 (quindi il bordo della finestra)
+	campo[yVisitors][69] = "|";
+	campo[yVisitors + 1][69]= "|";
+	campo[yVisitors + 2][69] = "|";
+	campo[yVisitors + 3][69] = "|";
+	_getch();
+}
+
 int main() {
 	system("cls");
 	for (a = 0; a < 20; a++) {                  //Inizializzazione Matrice
-			for (i = 0; i < 20; i++) {
+			for (i = 0; i < 70; i++) {
 				campo[a][i] = " ";
 			}
 		}
 	while (1) {
 		for (a = 0; a < 20; a++) {              //Stampa Matrice
-			for (i = 0; i < 20; i++) {
+			for (i = 0; i < 70; i++) {
 				cout << campo[a][i];
 			}
 			cout << endl;
 		}
 		for (a = 0; a < 20; a++) {              //Reset Matrice
-			for (i = 0; i < 20; i++) {
+			for (i = 0; i < 70; i++) {
 				campo[a][i] = " ";
 			}
 		}
 		home();
+		visitors();
 		system("cls");
 	}
 	return 0;
